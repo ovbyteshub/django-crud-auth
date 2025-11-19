f18cdfdad586493269832b77fbd2fd94163a38d9
Omar Velazquez <ovelazquez.mx@gmail.com>
2025-11-18 19:23:23 -0600

    Creating Logout logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 # Create your views here.
 
@@ -36,3 +36,7 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks/tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
