17579f19fdb6fba60da2917da3466e864c6b3ae9
Omar Velazquez <ovelazquez.mx@gmail.com>
2025-11-18 18:12:43 -0600

    Errors handler

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {
             'form': UserCreationForm()
-            })
+        })
     else:
         if request.method == 'POST':
             if request.POST['password1'] == request.POST['password2']:
                 # Register user
                 try:
-                    user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                    user = User.objects.create_user(
+                        username=request.POST['username'], password=request.POST['password1'])
                     user.save()
-                    return HttpResponse("User created successfully")
+                    return redirect('tasks')
                 except:
-                    return HttpResponse("Username already exists")
+                    return render(request, 'signup.html', {
+                        'form': UserCreationForm(),
+                        'error': 'Username already taken. Please choose a different one.'})
             else:
-                return HttpResponse("Passwords do not match")
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    'error': 'Passwords do not match. Please try again.'})
 
+def tasks(request):
+    return render(request, 'tasks/tasks.html')
